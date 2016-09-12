bc8970bb26f6ff739c4d4a10fe170b77d254879d
edersonlrf <edersonluis.rf@gmail.com>
2016-09-12 18:54:54 -0300

    A2 inserindo produtos no banco de dados

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -3,9 +3,22 @@
         <?php
         $nome = $_GET["nome"];
         $preco = $_GET["preco"];
-        ?>
-        <p class="alert-success">
-		    Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!
-		</p>
+
+        // Interpolar string e variavel usando o { }.
+        $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+
+        $conexao = mysqli_connect('localhost', 'root', 'usbw', 'php-004-loja');
+		mysqli_query($conexao, $query);
+		if(mysqli_query($conexao, $query)) {
+		?>
+		<p class="alert-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+		<?php
+		} else {
+		?>
+		<p class="alert-danger">O produto <? = $nome; ?> não foi adicionado</p>
+		<?php
+		}
+		mysqli_close($conexao);
+		?>
 
 <?php include("rodape.php"); ?>
