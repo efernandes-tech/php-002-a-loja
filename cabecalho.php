5f44357416c4150469f8787fde394cfe83e6da36
EdersonLRF <edersonluis.rf@gmail.com>
2016-09-17 01:22:44 -0300

    A4 listando todos os produtos

--- cabecalho.php
+++ cabecalho.php
@@ -15,6 +15,7 @@
             <div>
                 <ul class="nav navbar-nav">
                     <li><a href="produto-formulario.php">Adiciona Produto</a></li>
+                    <li><a href="produto-lista.php">Lista Produto</a></li>
                     <li><a href="sobre.php">Sobre</a></li>
                 </ul>
             </div>
