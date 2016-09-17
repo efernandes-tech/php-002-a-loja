cc2f99218119763f735f0516485e20ed9b50057e
edersonlrf <edersonluis.rf@gmail.com>
2016-09-17 16:44:10 -0300

    A6 ajustes

--- remove-produto.php
+++ remove-produto.php
@@ -7,5 +7,5 @@ removeProduto($conexao, $id);
 
 // Pagina que o browser deve redirecionar.
 header("Location: produto-lista.php?removido=true");
-// Uso indicado apos o redirecionamento para evitar vasar dados criticos.
+// Uso indicado apos o redirecionamento para evitar vazar dados criticos.
 die();
