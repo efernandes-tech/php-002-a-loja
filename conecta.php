6718d4f7af5bce75e7645e28a99a3edb7151559c
edersonlrf <edersonluis.rf@gmail.com>
2016-09-17 20:58:35 -0300

    A11 subindo a aplicacao para um servidor

--- conecta.php
+++ conecta.php
@@ -1,2 +1,3 @@
 <?php
 $conexao = mysqli_connect("localhost", "root", "usbw", "php-004-loja");
+// $conexao = mysqli_connect("mysql.hostinger.com.br", "u748295031_loja", "lojaloja", "u748295031_loja");
