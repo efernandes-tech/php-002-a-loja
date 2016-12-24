7c038f22b0db56a08a7ebfbc78ec61e7ec1617e2
edersonlrf <edersonluis.rf@gmail.com>
2016-12-24 21:56:51 -0200

    2A1 uma acao de login

--- index.php
+++ index.php
@@ -2,4 +2,37 @@
 
 <h1>Bem vindo!</h1>
 
+<?php
+if (isset($_GET["login"]) && $_GET["login"] == true) {
+?>
+    <p class="alert-success">Logado com sucesso!</p>
+<?php
+}
+?>
+<?php
+if (isset($_GET["login"]) && $_GET["login"] == false) {
+?>
+<p class="alert-danger">Usuário ou senha inválida!</p>
+<?php
+}
+?>
+
+<h2>Login</h2>
+
+<form action="login.php" method="post">
+    <table class="table">
+        <tr>
+            <td>Email</td>
+            <td><input class="form-control" type="email" name="email"></td>
+        </tr>
+        <tr>
+            <td>Senha</td>
+            <td><input class="form-control" type="password" name="senha"></td>
+        </tr>
+        <tr>
+            <td><button type="submit" class="btn btn-primary">Login</button></td>
+        </tr>
+    </table>
+</form>
+
 <?php include("rodape.php"); ?>
