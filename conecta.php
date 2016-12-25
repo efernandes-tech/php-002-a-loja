8618ee4327c2ded917defaf25967749d8773a14a
edersonlrf <edersonluis.rf@gmail.com>
2016-12-24 22:11:48 -0200

    alteracao

--- conecta.php
+++ conecta.php
@@ -1,3 +1,5 @@
 <?php
+
 $conexao = mysqli_connect("localhost", "root", "usbw", "php-002-a-loja");
+
 // $conexao = mysqli_connect("mysql.hostinger.com.br", "u748295031_loja", "lojaloja", "u748295031_loja");
