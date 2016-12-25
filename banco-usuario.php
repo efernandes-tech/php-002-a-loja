0b4125595cd877aa436f9bb9ed70d47447e449f2
EdersonLRF <edersonluis.rf@gmail.com>
2016-12-24 22:11:48 -0200

    alteracao

--- banco-usuario.php
+++ banco-usuario.php
@@ -1,7 +1,7 @@
 <?php
 
 function buscaUsuario($conexao, $email, $senha) {
-    // Criptografa a senha para verificar com a senha criptografada no banco.
+    // Criptografa a senha recebida para verificar com a senha criptografada no banco.
     $senhaMd5 = md5($senha);
 
     $query = "SELECT * FROM usuarios WHERE email='{$email}' AND senha='{$senhaMd5}'";
