65a144ca33d3dbe23c18bdfcfa6e14ddfd1f2f93
EdersonLRF <edersonluis.rf@gmail.com>
2016-12-26 21:34:41 -0200

    correcao

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php include("cabecalho.php"); ?>
 
+<?php
+if (isset($_GET["login"]) && $_GET["login"] == true) {
+?>
+    <p class="alert-success">Logado com sucesso!</p>
+<?php
+}
+?>
+<?php
+if (isset($_GET["login"]) && $_GET["login"] == false) {
+?>
+<p class="alert-danger">Usuário ou senha inválida!</p>
+<?php
+}
+?>
+
 <h1>Bem vindo!</h1>
 
 <?php
