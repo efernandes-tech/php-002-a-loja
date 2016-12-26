2d84d7791fccda6e2620a70be246283f7fb30128
edersonlrf <edersonluis.rf@gmail.com>
2016-12-26 21:18:40 -0200

    2A2 efetuando o login e cookies

--- login.php
+++ login.php
@@ -9,6 +9,8 @@ $usuario = buscaUsuario($conexao, $_POST["email"], $_POST["senha"]);
 if($usuario == null) {
     header("Location: index.php?login=0");
 } else {
+    setcookie("usuario_logado", $usuario["email"], time()+60);
+
     header("Location: index.php?login=1");
 }
 
