936ad4e282deed3cc5f21c2b92a671d426ba9ed7
EdersonLRF <edersonluis.rf@gmail.com>
2016-12-27 22:45:51 -0200

    2A8 sql injection

--- banco-usuario.php
+++ banco-usuario.php
@@ -4,6 +4,9 @@ function buscaUsuario($conexao, $email, $senha) {
     // Criptografa a senha recebida para verificar com a senha criptografada no banco.
     $senhaMd5 = md5($senha);
 
+    // Escapar caracteres especiais.
+    $email = mysqli_real_escape_string($conexao, $email);
+
     $query = "SELECT * FROM usuarios WHERE email='{$email}' AND senha='{$senhaMd5}'";
 
     $resultado = mysqli_query($conexao, $query);
