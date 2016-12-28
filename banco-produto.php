88aa21e9710b2caf5122fee5a7c8cfe312d0ec10
EdersonLRF <edersonluis.rf@gmail.com>
2016-12-27 22:52:19 -0200

    2A8 escapando queries

--- banco-produto.php
+++ banco-produto.php
@@ -13,6 +13,13 @@ function listaProdutos($conexao) {
 }
 
 function insereProduto($conexao, $nome, $preco, $descricao, $categoria_id, $usado) {
+    // Escapar caracteres especiais.
+    $nome = mysqli_real_escape_string($conexao, $nome);
+    $preco = mysqli_real_escape_string($conexao, $preco);
+    $descricao = mysqli_real_escape_string($conexao, $descricao);
+    $categoria_id = mysqli_real_escape_string($conexao, $categoria_id);
+    $usado = mysqli_real_escape_string($conexao, $usado);
+
     // Interpolar string e variavel usando o { }.
     $query = "INSERT INTO produtos (nome, preco, descricao, categoria_id, usado) VALUES ('{$nome}', '{$preco}', '{$descricao}', '{$categoria_id}', '{$usado}')";
 
@@ -24,18 +31,32 @@ function insereProduto($conexao, $nome, $preco, $descricao, $categoria_id, $usad
 }
 
 function alteraProduto($conexao, $id, $nome, $preco, $descricao, $categoria_id, $usado) {
+    // Escapar caracteres especiais.
+    $id = mysqli_real_escape_string($conexao, $id);
+    $nome = mysqli_real_escape_string($conexao, $nome);
+    $preco = mysqli_real_escape_string($conexao, $preco);
+    $descricao = mysqli_real_escape_string($conexao, $descricao);
+    $categoria_id = mysqli_real_escape_string($conexao, $categoria_id);
+    $usado = mysqli_real_escape_string($conexao, $usado);
+
     $query = "UPDATE produtos SET nome = '{$nome}', preco = {$preco}, descricao = '{$descricao}', categoria_id= {$categoria_id}, usado = {$usado} WHERE id = '{$id}'";
 
     return mysqli_query($conexao, $query);
 }
 
 function removeProduto($conexao, $id) {
+    // Escapar caracteres especiais.
+    $id = mysqli_real_escape_string($conexao, $id);
+
     $query = "DELETE FROM produtos WHERE id = {$id}";
 
     return mysqli_query($conexao, $query);
 }
 
 function buscaProduto($conexao, $id) {
+    // Escapar caracteres especiais.
+    $id = mysqli_real_escape_string($conexao, $id);
+
     $query = "SELECT * FROM produtos WHERE id = {$id}";
 
     $resultado = mysqli_query($conexao, $query);
