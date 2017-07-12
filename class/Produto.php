c8a7546a3bf84a375cb471bd7e59b4e7123b4f10
EdersonLRF <edersonluis.rf@gmail.com>
2017-07-11 22:52:19 -0300

    3A1 criando a classe produto

---
+++ class/Produto.php
@@ -0,0 +1,14 @@
+<?php
+
+class Produto {
+
+    public $id;
+    public $nome;
+    public $preco;
+    public $descricao;
+    public $categoria_id;
+    public $usado;
+
+}
+
+// arquivo class/Produto.php
