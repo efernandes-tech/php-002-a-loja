b5ec2d2ff0bbeee642c5a3e7a0b64320956d4ca0
EdersonLRF <edersonluis.rf@gmail.com>
2017-07-12 21:50:21 -0300

    3A2 evoluindo formulario base

--- produto-formulario-base.php
+++ produto-formulario-base.php
@@ -1,15 +1,18 @@
 <tr>
     <td>Nome</td>
-    <td><input type="text" name="nome" value="<?=$produto['nome']?>" class="form-control" /></td>
+    <td><input type="text" name="nome" value="<?=$produto->nome?>" class="form-control" /></td>
 </tr>
 <tr>
     <td>Preço</td>
-    <td><input type="number" name="preco" value="<?=$produto['preco']?>" class="form-control" /></td>
+    <td><input type="number" name="preco" value="<?=$produto->preco?>" class="form-control" /></td>
 </tr>
 <tr>
     <td>Descrição</td>
-    <td><textarea name="descricao" class="form-control"><?=$produto['descricao']?></textarea></td>
+    <td><textarea name="descricao" class="form-control"><?=$produto->descricao?></textarea></td>
 </tr>
+<?php
+$usado = $produto->usado ? "checked='checked'" : "";
+?>
 <tr>
     <td></td>
     <td><input type="checkbox" name="usado" <?=$usado?> value="true"> Usado
@@ -19,13 +22,13 @@
     <td>
         <select name="categoria_id" class="form-control">
         <?php foreach($categorias as $categoria) :
-            $essaEhACategoria = $produto['categoria_id'] == $categoria->id;
+            $essaEhACategoria = $produto->categoria->id == $categoria->id;
             $selecao = $essaEhACategoria ? "selected='selected'" : "";
         ?>
             <option value="<?=$categoria->id?>" <?=$selecao?>>
                 <?=$categoria->nome?>
             </option>
-        <?php endforeach ?>
+        <?php endforeach; ?>
         </select>
     </td>
 </tr>
