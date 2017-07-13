88015475a4d4bfb05ec8de9c4a497cafd0bd5646
EdersonLRF <edersonluis.rf@gmail.com>
2017-07-12 21:48:01 -0300

    3A2 alterando a listagem de categorias

--- produto-formulario-base.php
+++ produto-formulario-base.php
@@ -19,11 +19,11 @@
     <td>
         <select name="categoria_id" class="form-control">
         <?php foreach($categorias as $categoria) :
-            $essaEhACategoria = $produto['categoria_id'] == $categoria['id'];
+            $essaEhACategoria = $produto['categoria_id'] == $categoria->id;
             $selecao = $essaEhACategoria ? "selected='selected'" : "";
         ?>
-            <option value="<?=$categoria['id']?>" <?=$selecao?>>
-                <?=$categoria['nome']?>
+            <option value="<?=$categoria->id?>" <?=$selecao?>>
+                <?=$categoria->nome?>
             </option>
         <?php endforeach ?>
         </select>
