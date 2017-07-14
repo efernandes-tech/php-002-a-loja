54f4ac3928238d503108626b828ad2804f129076
EdersonLRF <edersonluis.rf@gmail.com>
2017-07-13 22:46:53 -0300

    3A4 criando getters e setters da classe produto

--- class/Produto.php
+++ class/Produto.php
@@ -9,6 +9,54 @@ class Produto {
     private $categoria;
     private $usado;
 
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getPreco() {
+        return $this->preco;
+    }
+
+    public function setPreco($preco) {
+        $this->preco = $preco;
+    }
+
+    public function getDescricao() {
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao) {
+        $this->descricao = $descricao;
+    }
+
+    public function getCategoria() {
+        return $this->categoria;
+    }
+
+    public function setCategoria($categoria) {
+        $this->categoria = $categoria;
+    }
+
+    public function getUsado() {
+        return $this->usado;
+    }
+
+    public function setUsado($usado) {
+        $this->usado = $usado;
+    }
+
     /**
      * @param float $valor 0.1 é o percentual padrão.
      */
