d1c2856a73cf305b80eab888ae39f119467a1b7b
edersonlrf <edersonluis.rf@gmail.com>
2017-07-13 22:16:06 -0300

    3A3 desconto e valor padrao

--- produto-lista.php
+++ produto-lista.php
@@ -17,6 +17,7 @@ $produtos = listaProdutos($conexao);
         <tr>
             <td><?= $produto->nome ?></td>
             <td><?= $produto->preco ?></td>
+            <td><?= $produto->precoComDesconto(); ?></td>
             <td><?= substr($produto->descricao, 0, 40) ?></td>
             <td><?= $produto->categoria->nome ?></td>
             <td><a class="btn btn-primary" href="produto-altera-formulario.php?id=<?=$produto->id?>">alterar</a></td>
