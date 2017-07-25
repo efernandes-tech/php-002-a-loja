ec3cfe89fd3752ccc86466367a7b94aa47435584
edersonlrf <edersonluis.rf@gmail.com>
2017-07-25 13:21:02 -0300

    4A3 criando a classe livro

---
+++ class/Livro.php
@@ -0,0 +1,17 @@
+<?php
+
+class Livro extends Produto {
+
+    private $isbn;
+
+    public function getIsbn() {
+        return $this->isbn;
+    }
+
+    public function setIsbn($isbn) {
+        $this->isbn = $isbn;
+    }
+
+}
+
+// class/Livro.php
